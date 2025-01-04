fc5d5eff4d8c70fa5993e6d5ef369e7a8259a55e
Anuj <invincibleanuj1718@gmail.com>
2025-01-04 17:56:00 +0530

    Navbar modified

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,7 +1,13 @@
+import Navbar from "@/components/Navbar";
 import React, { ReactNode } from "react";
 
 function layout({ children }: { children: React.ReactNode }) {
-  return <div className="relative flex h-screen w-full flex-col">{children}</div>;
+  return (
+    <div className="relative flex h-screen w-full flex-col">
+        <Navbar/>
+      <div>{children}</div>
+    </div>
+  );
 }
 
 export default layout;
--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,12 +1,12 @@
 import { PiggyBank } from "lucide-react";
 import React from "react";
 
-function logo() {
+export function Logo() {
   return (
     <a href="/" className="flex items-center gap-2">
       <PiggyBank className="stroke h-11 w-11 stroke-amber-500 stroke-[1.5]" />
       <p
-        className="bg-gradient-to-r from-blue-500 to-gray-800 bg-clip-text text-3xl font-bold 
+        className="bg-gradient-to-r from-blue-500 to-yellow-600 bg-clip-text text-3xl font-bold 
       leading-tight tracking-tighter text-transparent"
       >
         Track-Treasure
@@ -14,5 +14,15 @@ function logo() {
     </a>
   );
 }
-
-export default logo;
+export function LogoMobile() {
+  return (
+    <a href="/" className="flex items-center gap-2">
+      <p
+        className="bg-gradient-to-r from-blue-500 to-yellow-600 bg-clip-text text-3xl font-bold 
+      leading-tight tracking-tighter text-transparent"
+      >
+        Track-Treasure
+      </p>
+    </a>
+  );
+}
